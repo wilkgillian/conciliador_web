d2b53b54cafedd7d89ef4ac73fda277801ece4d9
wilk.gillian <wilk.silva@mt.senac.br>
2022-10-07 12:55:26 -0300

    testing api

--- src/pages/Animation.tsx
+++ src/pages/Animation.tsx
@@ -1,8 +1,17 @@
 import { Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import Lottie from "react-lottie";
 import animationData from "../components/Animation/background.json";
+import { api } from "../services/api";
 
 function Animation() {
+  const [dados, setDados] = useState([
+  //   {
+  //   id: 0,
+  //   text: "",
+  //   completed: false,
+  // }
+]);
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -11,10 +20,24 @@ function Animation() {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+  useEffect(() => {
+    const getDados = async () => {
+      const response = await api.get(
+        "/notes/"
+      );
+      const data = response.data
+      setDados(response.data);
+      console.log(data);
+    };
+    getDados();
+  }, []);
   return (
-    <Box w="100%" h="100%">
-      <Lottie options={defaultOptions} />
-    </Box>
+    <>
+      <Box w="100vw" h="100vh" position="absolute">
+        <Lottie options={defaultOptions} />
+      </Box>
+      <Box></Box>
+    </>
   );
 }
 
